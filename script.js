a595b5890e5f6473f6a5eeb7e7526e90a01da348
Sidharh1998 <116628665+Sidharh1998@users.noreply.github.com>
2023-05-01 13:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-//your code here
-function SquidGameII(n, Arr) {
-  let cnt = [0, 0, 0];
-	for (let i = 0; i < n; i = i + 1) {
-		cnt[Arr[i] % 3] += 1;
-	}
-	//console.log(cnt)
-	if (cnt[0] % 2 === 0) {
-	return cnt[1] > 0 && cnt[2] > 0;
-	}
-	return Math.abs(cnt[1] - cnt[2]) > 2;
-}
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener('input', () => {
+	let str = evaluatedText.value;
+	letterCount.innerHTML = `${str.length}`;
+});
