8be62d03a5f5cea9e8feb9682aac5262c1c08854
Sidharh1998 <116628665+Sidharh1998@users.noreply.github.com>
2023-05-01 12:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function SquidGameII(n, Arr) {
+  let cnt = [0, 0, 0];
+	for (let i = 0; i < n; i = i + 1) {
+		cnt[Arr[i] % 3] += 1;
+	}
+	//console.log(cnt)
+	if (cnt[0] % 2 === 0) {
+	return cnt[1] > 0 && cnt[2] > 0;
+	}
+	return Math.abs(cnt[1] - cnt[2]) > 2;
+}
